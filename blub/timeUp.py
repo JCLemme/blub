6e160b60696c97037c5da9a80e2b2e5f31a0d632
NoahNicolella <noah.nicolella@gmail.com>
2020-03-13 13:50:04 -0400

    Added stuff to python script.

--- blub/timeUp.py
+++ blub/timeUp.py
@@ -1,1 +1,19 @@
 #This gon be a timeup script
+import os
+import asyncio
+import websockets
+import json
+ 
+async def log_off(websocket, path):
+    uri = "ws://johnlemmetown.com"
+    async with websockets.connect(uri) as websocket:
+        ans = await websocket.recv()
+
+        ans_cool = json.loads(ans)
+        
+
+# Prints the thign
+print("\nYou are being logged out...")
+
+# Logs user out
+os.system("shutown -l")
