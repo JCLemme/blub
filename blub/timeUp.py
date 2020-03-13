b714f81e0e43652573de097c2c81976bf64c1e6d
NoahNicolella <noah.nicolella@gmail.com>
2020-03-13 14:13:02 -0400

    Added to py, created websocket system.

--- blub/timeUp.py
+++ blub/timeUp.py
@@ -10,18 +10,18 @@ username = os.getlogin()
 async def log_off(websocket, path):
     uri = "ws://johnlemmetown.com"
     async with websockets.connect(uri) as websocket:
-        message = {'logged_on': True, 'username': username}
-        message_json = json.dump(message)
 
-        await websocket.send(message_json)
-        ans = await websocket.recv()
-
-        ans_cool = json.loads(ans)
+        task = await websocket.recv()
+        task_json = json.loads(task)
+        
+        if task_json[task] == 'info':
+            message = {'logged_on': True, 'username': username}
+            message_json = json.dump(message)
+            await websocket.send(message_json)
+        elif task_json[task] == 'kill':
+            print("\nYou are being logged out...")
+            os.system("shutown -l")
         
 
-# Prints the thign
-print("\nYou are being logged out...")
 pymsgbox.alert(text='test', title='test', button='OK')
 
-# Logs user out
-os.system("shutown -l")
