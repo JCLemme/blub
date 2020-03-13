aa0c36153e4ea4726583f2d312551cbdaf510e29
NoahNicolella <noah.nicolella@gmail.com>
2020-03-13 14:06:11 -0400

    Added to py script.

--- blub/timeUp.py
+++ blub/timeUp.py
@@ -5,9 +5,15 @@ import asyncio
 import websockets
 import json
  
+username = os.getlogin()
+
 async def log_off(websocket, path):
     uri = "ws://johnlemmetown.com"
     async with websockets.connect(uri) as websocket:
+        message = {'logged_on': True, 'username': username}
+        message_json = json.dump(message)
+
+        await websocket.send(message_json)
         ans = await websocket.recv()
 
         ans_cool = json.loads(ans)
@@ -15,7 +21,7 @@ async def log_off(websocket, path):
 
 # Prints the thign
 print("\nYou are being logged out...")
-pymsgbox.alert(text='Your session has timed out. You will be logged out.', title='Session Over', button='OK')
+pymsgbox.alert(text='test', title='test', button='OK')
 
 # Logs user out
 os.system("shutown -l")
