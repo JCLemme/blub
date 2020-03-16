ed0f657922b775f6cf3ad39b6d9a8dcc08a4a9cf
NoahNicolella <noah.nicolella@gmail.com>
2020-03-16 12:30:45 -0400

    Made a few fixes.

--- blub/rdp_file.js
+++ blub/rdp_file.js
@@ -1,15 +1,20 @@
 const fs = require('fs')
 
 // This is just placeholder, we will obviously grab all of these from the backend of the rest of the site
-username = '';
+
+
+username = `ecc\\`
 password = '';
 address = '';
 
 content = '\uFEFF' + 
-`full address:s:<${address}>
-username:s:<${username}>
-password 51:b:<${password}>
+`full address:s:<'${address}'>
+username:s:<'${username}'>
+password:s:<'${password}'>
 `
 
 
-fs.writeFile(`connectionFile ${username}.rdp`, content, {encoding: 'utf8'});
+fs.writeFile(`connectionFile - ${username}.rdp`, content, function (err) {
+    if (err) throw err
+    console.log('done')
+});
