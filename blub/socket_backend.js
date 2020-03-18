4944b23e0ca892246f060050c05a890b38f9896b
John C Lemme <jclemme@proportionallabs.com>
2020-03-18 17:20:07 -0400

    Sockets work

--- blub/socket_backend.js
+++ blub/socket_backend.js
@@ -8,9 +8,9 @@ send = function(username, message) {
     if(username in _sockets) {
     
         for(var i=0;i<_sockets[username].length;i++) {
-            if(_sockets[username] instanceof websocket) {
-                if(_sockets[username].readyState == 1) {
-                    _sockets[username].send(message);
+            if(_sockets[username][i] instanceof websocket) {
+                if(_sockets[username][i].readyState == 1) {
+                    _sockets[username][i].send(message);
                 }
             }
         }
