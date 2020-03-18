0f5d0ec5a51883f78f33285654e59f581b16611a
John C Lemme <jclemme@proportionallabs.com>
2020-03-18 17:19:00 -0400

    Sockets work

--- blub/socket_backend.js
+++ blub/socket_backend.js
@@ -6,16 +6,23 @@ var _sockets = {};
 
 send = function(username, message) {
     if(username in _sockets) {
-        if(_sockets[username] instanceof websocket) {
-            if(_sockets[username].readyState == 1) {
-                _sockets[username].send(message);
+    
+        for(var i=0;i<_sockets[username].length;i++) {
+            if(_sockets[username] instanceof websocket) {
+                if(_sockets[username].readyState == 1) {
+                    _sockets[username].send(message);
+                }
             }
         }
+        
     }
 };
 
 register = function(username, socket) {
-    _sockets[username] = socket;
+    if(!(username in _sockets)) 
+        _sockets[username] = [];
+        
+    _sockets[username].add(socket);
 };
 
 module.exports.send = send;
